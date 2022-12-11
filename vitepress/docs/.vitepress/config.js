cf07fd7050b8e3b9504c271ed90c36a6e6c39ee8
Alan Rynne <alan@rynne.es>
2022-12-11 19:48:03 +0100

    Added initial vitepress site

---
+++ vitepress/docs/.vitepress/config.js
@@ -0,0 +1,17 @@
+export default {
+  title: "GH Size Analyzer",
+  description:
+    "A grasshopper component to analyze size of internal data in stored in the document.",
+  themeConfig: {
+    sidebar: [
+      {
+        text: "Getting started",
+        items: [
+          { text: "Install", link: "/install.md" },
+          { text: "Usage", link: "/usage.md" },
+          { text: "Settings", link: "/settings.md" }
+        ]
+      }
+    ]
+  }
+}
---
+++ vitepress/docs/index.md
@@ -0,0 +1,18 @@
+---
+layout: home
+
+hero:
+  name: GH Size Analyzer
+  text: Find the nodes that make your files huge.
+  tagline: Brought to you by Paramdigma
+  image:
+    src: /logo.png
+    alt: VitePress
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /install.md
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com/paramdigma/gh_sizeanalyzer
+---
